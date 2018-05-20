7d5255ee7cd64d99e64f235e2ab44914aa6cad1c
smith9718 <39279480+smith9718@users.noreply.github.com>
2018-05-20 18:33:58 +0700

    up

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '45nis0kjrGYqQ6Bsx4rK/RO4dTI4vRRy13FDI493RAfE1YwMDeHaxNHiV/VfxPf/lgHEK9RAW/G1hiSWE390jToDoZLe10ev8MbkuStt92M4ThhZPZt0NGXS5/FUUr42gwRm5lpaRHyx0IgcfxdEsgdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
